292a5b5ce98e4c5540dc3ebda18e8aadd7d344d1
JoseEduardo-max <jeduardoff2@gmail.com>
2025-04-03 21:38:28 -0300

    adicionei na pasta o arquivo Eduardo
    :ql

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Cadastro from "./pages/Cadastro";
+import Eduardo from "./pages/Eduardo";
 import Listar from "./pages/Listar";
 
 import { 
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<h1>Ola mundo</h1>}/>
         <Route path="/listar" element={<Listar/>}/>
         <Route path="/cadastro" element={<Cadastro/>}/>
+        <Route path="/pages/eduardo" element={<Eduardo/>}/>
       </Routes>
     </BrowserRouter>
   )
