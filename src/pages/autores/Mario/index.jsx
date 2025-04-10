5523ef29a69b0b8729528c0b5876ea5f827fb781
unknown <17marioneto17@gmail.com>
2025-04-10 20:38:59 -0300

    Subindo página do Mário

--- src/pages/autores/Mario/index.jsx
+++ src/pages/autores/Mario/index.jsx
@@ -1,9 +1,23 @@
 import "./styles.css";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 export default function Mario() {
     return (
         <div>
-            Ola, eu sou <span className="nome">Fulano</span>
+           
+            <Card variant="outlined" > 
+            <img src="https://unblast.com/wp-content/uploads/2021/01/Space-Background-Images.jpg" style={{ width: "100%", borderRadius: "8px" }} />  
+            <CardContent class="container">
+                <h1>Mário</h1>
+                <div style={{textAlign: 'right'}}>
+                    <img src="https://avatars.githubusercontent.com/u/185074209?v=4" class="avatar"/>
+                    <p>Diretor de Arte - UX/UI Designer</p>
+                </div>
+                
+            </CardContent>
+            </Card>
+    
         </div>
     )
 }
