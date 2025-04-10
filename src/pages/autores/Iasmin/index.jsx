92ada3a2471e0ab7cce4f5849c39f6a2b6a8d659
unknown <iasminsena6013@digitalcollege.internal>
2025-04-10 20:13:29 -0300

    Criação da página da Iasmin

--- src/pages/autores/Iasmin/index.jsx
+++ src/pages/autores/Iasmin/index.jsx
@@ -1,9 +1,26 @@
+import foto from "./img/iasmin.png"
 import "./styles.css";
+
 
 export default function Iasmin() {
     return (
         <div>
-            Ola, eu sou <span className="nome">Fulano</span>
+            <div className="perfil">
+                <div>
+                    <img  width= "100px" src={foto}/>
+                </div>
+                <div>
+                    Hi, my name is <br/>
+                    <span className="white">Iasmin Oliveira e Sena</span>
+                </div>
+            </div>
+            <div>
+                <h3>About Me</h3>
+                <p>
+                I love mixing graphic design with front-end to build beautiful, practical, and user-friendly interfaces. With a solid background in design and dev, I focus on the details to make everything smooth and intuitive. I'm always keeping up with trends and enjoy turning ideas into creative, functional projects.
+                </p>
+            </div>
+
         </div>
     )
 }
