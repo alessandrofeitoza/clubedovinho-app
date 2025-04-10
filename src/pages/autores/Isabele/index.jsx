980e7c2d2f957d7106cf2425988af9f353c8d9ff
unknown <isabeletm@gmail.com>
2025-04-10 20:13:35 -0300

    tela-isabele

--- src/pages/autores/Isabele/index.jsx
+++ src/pages/autores/Isabele/index.jsx
@@ -1,9 +1,19 @@
+import {Button} from "@mui/material";
+import minhaFotoDeMim from "./img/foto.jfif";
 import "./styles.css";
 
 export default function Isabele() {
     return (
         <div>
-            Ola, eu sou <span className="nome">Fulano</span>
-        </div>
+            <Button color="error" variant="contained">Instagram</Button>
+
+            <img width="100px" src={minhaFotoDeMim}/>
+            <br>
+            </br>
+
+            Ola, eu sou <span className="nome">Isabele</span>
+        </div>  
+        
+        
     )
 }
