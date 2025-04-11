630ec971a289a1b9ec7f6e6711a4507bae63fe17
ronneyrv <ronneyrv@gmail.com>
2025-04-10 21:34:49 -0300

    page-about-ronney
    esc
    :x

--- src/pages/autores/Ronney/index.jsx
+++ src/pages/autores/Ronney/index.jsx
@@ -1,9 +1,41 @@
-import "./styles.css";
+import "./styles.scss";
 
 export default function Ronney() {
     return (
-        <div>
-            Ola, eu sou <span className="nome">Ronney</span>
+        <div className="page-ronney">
+            <div className="about-container">
+                <div className="main">
+                    <div className="avatar">
+                        <img src="https://avatars.githubusercontent.com/u/135840486?s=400&u=e1b20891d9398e16443cf7dd8c0fc4552ea5387a&v=4" alt="ronney_profile" />
+                    </div>
+                    <div className="about-content">
+                        <h1>Dev Ronney</h1>
+                        <span>Estudante Full Stack | Digital College</span>
+                        <p>
+                            Olá, sou o Ronney, estudante de Desenvolvimento Full Stack na Digital College e apaixonado por criar aplicações web completas e de alta performance. Com uma base sólida em JavaScript e suas bibliotecas, estou constantemente aprimorando minhas habilidades para entregar projetos escaláveis, seguros e com boa experiência de usuário. Meu objetivo é integrar as melhores práticas de desenvolvimento e as tecnologias mais recentes para transformar ideias em soluções inovadoras.
+                        </p>
+                        <div className="skills">
+                            <h3>Linguagens e Ferramentas:</h3>
+                            <ul>
+                                <li>JavaScript (ES6+)</li>
+                                <li>React.js</li>
+                                <li>Node.js</li>
+                                <li>Git / GitHub</li>
+                            </ul>
+                        </div>
+                        <div className="skills">
+                            <h3>Skills:</h3>
+                            <ul>
+                                <li>Desenvolvimento de aplicações completas (front-end e back-end)</li>
+                                <li>Criação de interfaces responsivas e dinâmicas com React.js</li>
+                                <li>Desenvolvimento de APIs e sistemas no back-end com Node.js e Express</li>
+                                <li>Versionamento de código com Git</li>
+                                <li>Metodologias ágeis (Scrum)</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
