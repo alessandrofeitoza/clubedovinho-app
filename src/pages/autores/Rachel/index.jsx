6220cf5dc08731125f5c356c5853a64c3b909976
Alessandro Feitoza <Alessandro.Feitoza@digitalcollege.internal>
2025-04-10 21:13:04 -0300

    Criando minha pagina

--- src/pages/autores/Rachel/index.jsx
+++ src/pages/autores/Rachel/index.jsx
@@ -1,9 +1,44 @@
-import "./styles.css";
+import gatinho from "./img/gatinho.jpg";
+import { ChevronLeft } from "@mui/icons-material";
+import { Settings } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { AddCircle } from "@mui/icons-material";
+import "./styles.scss";
 
 export default function Rachel() {
     return (
-        <div>
-            Ola, eu sou <span className="nome">Fulano</span>
+        <div className="page-rachel">
+            <div  className="icons">
+                <ChevronLeft/>
+                <Settings/>
+            </div>
+
+            <div>
+                <img width="80px" className="imagem" src={gatinho}/>
+            </div>
+
+            <h2 className="titulo">Rachel Rocha</h2>
+            <h3 className="subtitulo">Fisioterapeuta</h3>
+
+
+
+           <div className="barra">
+
+            <Button>Following</Button>
+            <AddCircle className="addcircle"/>
+            <Button>Followers</Button>
+
+           </div>
+
+           <div className="conteudo">
+            
+           </div>
+
         </div>
+
+        
     )
+
+
+    
 }
