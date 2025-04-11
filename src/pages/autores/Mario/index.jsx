ca567b74467a9ca628ff5696315ace179db06ad1
unknown <17marioneto17@gmail.com>
2025-04-10 21:20:30 -0300

    pagina mario com os elementos organizados

--- src/pages/autores/Mario/index.jsx
+++ src/pages/autores/Mario/index.jsx
@@ -1,23 +1,26 @@
-import "./styles.css";
+import "./styles.scss";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
 export default function Mario() {
     return (
-        <div>
-           
-            <Card variant="outlined" > 
-            <img src="https://unblast.com/wp-content/uploads/2021/01/Space-Background-Images.jpg" style={{ width: "100%", borderRadius: "8px" }} />  
-            <CardContent class="container">
-                <h1>Mário</h1>
-                <div style={{textAlign: 'right'}}>
-                    <img src="https://avatars.githubusercontent.com/u/185074209?v=4" class="avatar"/>
-                    <p>Diretor de Arte - UX/UI Designer</p>
+        <div class="page-mario">
+            <Card variant="outlined">
+                <img src="https://unblast.com/wp-content/uploads/2021/01/Space-Background-Images.jpg" style={{ width: "100%", borderRadius: "8px" }} />
+                <CardContent className="container">
+                    <div className="text-container">
+                        <h1>Mário</h1>
+                        <p>Diretor de Arte - UX/UI Designer</p>
+                    </div>
+                    <img src="https://avatars.githubusercontent.com/u/185074209?v=4" className="avatar" />
+                </CardContent>
+                
+                <div>
+                    <button href="#"> Entrar em contato </button>
                 </div>
                 
-            </CardContent>
+                
             </Card>
-    
         </div>
-    )
+    );
 }
