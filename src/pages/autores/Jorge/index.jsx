c92a9ddf22d03fe7f1dbff08f2f0dc95d08803ed
Jorge Eduardo <jeasdigitalcollege@outlook.com>
2025-04-24 19:52:28 -0300

    Adicionando SASS ao projeto

--- src/pages/autores/Jorge/index.jsx
+++ src/pages/autores/Jorge/index.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@mui/material";
 import { ArrowRight } from "@mui/icons-material";
-import styles from "./styles.module.css";
+import styles from "./styles.module.scss";
 import foto from "./img/foto.jpg";
 
 export default function ProfileCard() {
