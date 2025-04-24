b4513adc7cd5c0316e9946ed8977986148a71e70
daviiisousa <sousadavi248@gmail.com>
2025-04-24 19:53:43 -0300

    init

---
+++ src/pages/autores/davi/index.jsx
@@ -0,0 +1,27 @@
+import daviFoto from "./assets/imgs/davi-perfil.webp";
+import instagramIcon from "./assets/icons/insta-icon.png";
+import githubIcon from "./assets/icons/github-icon.png"
+import "./stely.scss";
+
+export default function Davi() {
+  return (
+    <div className="page-davi">
+      <h1>Davi Sousa Alves</h1>
+      <img className="fotoDePerfil" src={daviFoto} alt="minha foto de perfil" />
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.instagram.com/daviiisousa19/" target="_blanck">
+              <img className="instIcon" src={instagramIcon} alt="icone do insta" />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/daviiisousa" target="_blanck">
+                <img src={githubIcon} alt="icone gitHub" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
