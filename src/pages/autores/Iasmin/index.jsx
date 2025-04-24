6fcbdce29d62f38eedadbe1b0863deb0e7e33fd3
unknown <iasminsena6013@digitalcollege.internal>
2025-04-24 20:42:15 -0300

    Finalização da minha página

--- src/pages/autores/Iasmin/index.jsx
+++ src/pages/autores/Iasmin/index.jsx
@@ -1,20 +1,13 @@
-import foto from "./img/iasmin.png";
-import "./styles.scss";
 import foto from "./img/iasmin.png"
-import {Card} from "@mui/material";
-import "./styles.css";
-
-
+import "./styles.scss";
 export default function Iasmin() {
     return (
         <div className="pages-iasmin">
+
             <div className="perfil">
                 <div>
                     Olá, me chamo <br/>
                     <span className="white">Iasmin Oliveira e Sena</span>
-                </div>
-                <div>
-                    <img  width= "100px" src={foto}/>
                 </div>
                 <div>
                     <img  width= "100px" src={foto}/>
@@ -31,7 +24,8 @@ export default function Iasmin() {
                 <div>
                     <h3>Serviços</h3>
                     <div className="conteiner">
-                        <div className="card mb-4">
+
+                        <div className="card">
                             <h5>Analista de testes</h5>
                             <p>
                                 - Escrita de casos de teste <br/>
@@ -39,7 +33,7 @@ export default function Iasmin() {
                                 - Análise de requisitos <br/>
                                 - Desenvolvimento de um plano de teste </p>
                         </div>
-                        <div className="card mb-4">
+                        <div className="card">
                             <h5> Desenvolvedora Full Stack</h5>
                             <p>
                                 - Criação de site responsivo <br/>
@@ -63,6 +57,7 @@ export default function Iasmin() {
                                 - Diagnóstico e solução de problemas
                                 - Python, SQL, JD Edwards
                                 - Implementação de práticas de backup e recuperação de dados
+
                             </p>
                         </div>
                     </div>
@@ -79,67 +74,7 @@ export default function Iasmin() {
                         </div>
                         <div className="card_contatos">
                             <a href="https://www.linkedin.com/in/iasmin-oliveira-e-sena-945882200/">LinkedIn</a>
-                        </div>
-                    </div>
-                </div>
 
-                <h3>About Me</h3>
-                <p>
-                I love mixing graphic design with front-end to build beautiful, practical, and user-friendly interfaces. With a solid background in design and dev, I focus on the details to make everything smooth and intuitive. I'm always keeping up with trends and enjoy turning ideas into creative, functional projects.
-                </p>
-
-                <div>
-                    <h3>Services</h3>
-                    <div className="conteiner">
-                        <div className="card">
-                            <h5>Test analyst</h5>
-                            <p>
-                                - Test case writing <br/>
-                                - Usability testing <br/>
-                                - Requirements analysis <br/>
-                                - Develop a test plan</p>
-                        </div>
-                        <div className="card">
-                            <h5>Full Stack developer</h5>
-                            <p>
-                                - Creation of responsive website <br/>
-                                - HTML, CSS e JavaScript <br/>
-                                - Website performance & accessibility optimization
-                            </p>
-                        </div>
-                        <div className="card">
-                            <h5>User experience (UX) design</h5>
-                            <p>
-                                - UI needs research and analysis <br/>
-                                - Wireframing & prototyping <br/>
-                                - User testing & interations to improve overall experience
-                            </p>
-                        </div>
-                        <div className="card">
-                            <h5>Technical support</h5>
-                            <p>
-                                - Diagnosis and troubleshooting
-                                - Python, SQL, JD Edwards
-                                - Implement data backup and recovery practices
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <h3>Contatos</h3>
-                    <div>
-                        <div>
-                            <h5>GitHub</h5>
-                            <a href=""></a>
-                        </div>
-                        <div>
-                            <h5>Instagram</h5>
-                            <a href=""></a>
-                        </div>
-                        <div>
-                            <h5>LinkedIn</h5>
-                            <a href=""></a>
                         </div>
                     </div>
                 </div>
